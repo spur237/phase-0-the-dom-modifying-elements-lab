32300140be18a4ddad4a059d7b9b6cfccdc9f2ad
spur237 <samuel.nzioka@student.moringaschool.com>
2022-08-21 21:48:50 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.getElementById('main')
+element.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Charles is the champion"
+
+document.body.append(newHeader) 
